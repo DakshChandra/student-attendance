9ef218ef0e73e956662783a4c2b3fc82fc52dad3
DakshChandra <114349904+DakshChandra@users.noreply.github.com>
2023-10-03 19:05:21 +0530

    Attendance

---
+++ inc/footer.php
@@ -0,0 +1,11 @@
+	<div class="container mt-3">
+		<div class="card card-body bg-light text-center">
+			<h3>
+				<span>Copyright &copy;</span>
+				
+				<span>Dc</span>
+			</h3>
+		</div>
+	</div>
+</body>
+</html>
